4f74e70afe2ef35aca2a3685fc98aedcf14047a2
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-31 18:54:37 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,6 +25,7 @@ def find_greater_pair(src)
 end
 
 def total_even_pairs(src)
+  binding.pry
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
