e6722415ab07706d39fa59938008388d748cf2df
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-31 19:18:59 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -30,10 +30,10 @@ def total_even_pairs(src)
    even_numbers = src.select {|number_pair| number_pair[0].even? && number_pair[1].even? }
 
   #  Since we have a nested array, we need to 'flatten' them into a single dimensional array, no more array of arrays
-   flattened_array = even_numbers.flatten
+   #flattened_array = even_numbers.flatten
 
   #  This sets up the variable that we are going to return as we loop thru our flattened array and sum all of our numbers.
-   total_even_sum = 0
+   #total_even_sum = 0
 
   #  This each loop, loops over each number and adds it to the total_even_sum variable
    flattened_array.each do |num|
